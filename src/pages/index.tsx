9dc7a7312019741a502e03adfe8c567a1ed180c1
Erick <libertti@live.com>
2021-04-19 11:49:01 -0300

    Painel login finalizado - Chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,66 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title> DashGo | Home </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Flex 
+        w="100vw"
+        h="100vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+         <Flex 
+          as="form"
+          w="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDirection="column"
+         >
+          <Stack spacing="4">
+          
+          <FormControl>
+            <FormLabel htmlForm="email">E-mail</FormLabel>
+            <Input 
+              id="email"
+              name="email" 
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
 
-      <main >
-          hellow world
-      </main>
+          <FormControl>
+            <FormLabel htmlForm="password">Password</FormLabel>
+            <Input
+              id="password"
+              name="password" 
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          </Stack>
+          <Button type="submit" mt={6} colorScheme="pink" size="lg">Entrar</Button>
+         </Flex>
+      </Flex>
     </div>
   )
 }
